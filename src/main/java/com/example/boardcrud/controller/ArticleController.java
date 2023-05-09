4233cec757db7b23ce81566dcc5114dd641463d1
dongwook <kdw950108@gmail.com>
2023-05-10 01:47:09 +0900

    add : delete

--- src/main/java/com/example/boardcrud/controller/ArticleController.java
+++ src/main/java/com/example/boardcrud/controller/ArticleController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -98,6 +99,24 @@ public class ArticleController {
 
         //3. 수정 결과 페이지로 리다이렉트
         return "redirect:/articles/" + articleEntity.getId();
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        log.info("삭제 요청 들어왔나요");
+
+        //1. 삭제 대상을 가져온다.
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info("target={}", target);
+
+        //2. 대상을 삭제한다.
+        if (target != null) {
+            articleRepository.delete(target);
+            redirectAttributes.addFlashAttribute("message", "삭제완료!!");
+        }
+
+        //3. 결과페이지로 리다이렉트
+        return "redirect:/articles";
     }
 
 }
