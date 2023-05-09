7613e18b88a3ff112f57cdf7c028313d41213877
dongwook <kdw950108@gmail.com>
2023-05-09 20:43:06 +0900

    add : form 데이터 주고받기 1. view -> form 생성 2. form 이름 dto 필드명 이름 동일

---
+++ src/main/java/com/example/boardcrud/dto/ArticleForm.java
@@ -0,0 +1,22 @@
+package com.example.boardcrud.dto;
+
+
+public class ArticleForm {
+
+    private String title;
+
+    private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
