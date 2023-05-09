f6cab11afe30368689488a67ade36617b67cf3e2
dongwook <kdw950108@gmail.com>
2023-05-10 00:31:00 +0900

    add : 수정 폼 만들기

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO ARTICLE(id, title, content)
+VALUES (1, '치킨', '맛도리');
+
+INSERT INTO ARTICLE(id, title, content)
+VALUES (2, '햄버거', '맛도리');
+
+INSERT INTO ARTICLE(id, title, content)
+VALUES (3, '피자', '맛도리');
+
