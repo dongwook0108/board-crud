d07fbb8b0c9f67302e3b2a7de0c650ac7a4d64eb
dongwook <kdw950108@gmail.com>
2023-05-10 17:45:49 +0900

    add : h2 데이터 베이스 기본설정

--- src/main/java/com/example/boardcrud/entity/Article.java
+++ src/main/java/com/example/boardcrud/entity/Article.java
@@ -4,21 +4,23 @@ package com.example.boardcrud.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Entity
+@Entity // 해당 클래스로 테이블을 만든다 !
 @AllArgsConstructor
 @ToString
 @NoArgsConstructor
 @Getter
 public class Article {
+
     // 왜 NoArgsConstructor 필요?? -> JPA에서 기본적으로 디폴트 생성자 요구
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // db가 알아서 생성
     private Long id;
 
     @Column
